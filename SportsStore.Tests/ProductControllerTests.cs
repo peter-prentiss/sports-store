c8dbf66b3dd525755197fdbcdffa87a6ef1a3442
Peter <peter.prentiss@gmail.com>
2018-03-30 12:41:44 -0500

    Add pagination and unit testing for pagination

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void CanPaginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+            Product[] productArray = result.ToArray();
+            Assert.True(productArray.Length == 2);
+            Assert.Equal("P4", productArray[0].Name);
+            Assert.Equal("P5", productArray[1].Name);
+        }
+    }
+}
