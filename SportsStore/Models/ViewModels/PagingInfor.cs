5aad307d3627b4c56da59c8455783a0f94743af8
Peter <peter.prentiss@gmail.com>
2018-03-30 12:55:35 -0500

    Create tag helper class

---
+++ SportsStore/Models/ViewModels/PagingInfor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class PagingInfor
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    }
+}
